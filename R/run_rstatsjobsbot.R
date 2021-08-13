c3f42d35da2fd8a7aa4d5d9b075e71bd5d2248cf
Juan Cruz Rodriguez <jcrodriguez@unc.edu.ar>
2021-08-13 08:44:20 -0300

    skipping blocked accounts

--- R/run_rstatsjobsbot.R
+++ R/run_rstatsjobsbot.R
@@ -11,6 +11,7 @@
 #' @param from_time A POSIXct indicating minimum tweets time to attend.
 #' @param max_hashtags Numeric indicating the max number of hashtags that a
 #'   tweet can have (removed if has more).
+#' @param blocked A character vector with the `screen_name`s of blocked accounts (not to retweet).
 #'
 #' @importFrom rtweet create_token
 #'
@@ -23,7 +24,8 @@ run_rstatsjobsbot <- function(rtweet_app,
                                 rtweet_access_secret,
                                 user = "RStatsJobsBot",
                                 from_time = Sys.time(),
-                                max_hashtags = 15) {
+                                max_hashtags = 15,
+                                blocked = c()) {
   # Set Twitter credentials.
   create_token(
     app = rtweet_app, consumer_key = rtweet_consumer_key, consumer_secret = rtweet_consumer_secret,
@@ -31,7 +33,7 @@ run_rstatsjobsbot <- function(rtweet_app,
   )
   message(paste0("user: ", user))
   # Get posts that should be retweeted.
-  rtable_posts <- get_rtable_posts(user, from_time, max_hashtags)
+  rtable_posts <- get_rtable_posts(user, from_time, max_hashtags, blocked)
   message(paste0(Sys.time(), " - ", nrow(rtable_posts), " tweets to rt."))
   # Update from_time.
   from_time <- Sys.time()
@@ -50,12 +52,13 @@ run_rstatsjobsbot <- function(rtweet_app,
 #' @param from_time A POSIXct indicating minimum tweets time to attend.
 #' @param max_hashtags Numeric indicating the max number of hashtags that a
 #'   tweet can have (removed if has more).
+#' @param blocked A character vector with the `screen_name`s of blocked accounts (not to retweet).
 #'
 #' @importFrom dplyr `%>%` anti_join arrange distinct filter
 #' @importFrom rtweet get_timeline search_tweets
 #' @importFrom stringr str_count
 #'
-get_rtable_posts <- function(user, from_time, max_hashtags) {
+get_rtable_posts <- function(user, from_time, max_hashtags, blocked) {
   # Avoid R CMD check warnings.
   reply_to_screen_name <- screen_name <- created_at <- is_retweet <- text <- NULL
   # Get tweets with my username, and newer than from_time.
@@ -63,7 +66,9 @@ get_rtable_posts <- function(user, from_time, max_hashtags) {
     search_tweets(user, type = "recent", include_rts = FALSE) %>%
       # Remove replies to me, or posts written by me.
       filter(!reply_to_screen_name %in% user & !screen_name %in% user) %>%
-      filter(created_at > from_time)
+      filter(created_at > from_time) %>% 
+      # Remove blocked accounts.
+      filter(!screen_name %in% blocked)
   })
   # If there was an error (internet mostly) return an empty data.frame .
   if (inherits(mentions, "try-error")) {
